873655413e878a8cf4ecd16680487b2f3da5a27a
neivyx <janeivyhilario@gmail.com>
2020-07-11 05:06:23 -0400

    require relative from lib/rbs

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/version"
+require_relative "../lib/api.rb"
+require_relative "../lib/cli.rb"
+require_relative "../lib/facts.rb"
+
+
+
+# module PuertoRicoFacts
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
