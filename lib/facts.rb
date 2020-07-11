8b98577ba435601f5dc226b1b748e28b31db42c4
neivyx <janeivyhilario@gmail.com>
2020-07-11 07:10:10 -0400

    added attr/init/self method

--- lib/facts.rb
+++ lib/facts.rb
@@ -1,3 +1,15 @@
 class Facts
-    
+
+    attr_accessor :fact
+
+    @@all = []
+
+    def initialize(fact)
+        @fact = fact
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
