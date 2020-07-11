00500c8c28806b1edc9dba240d3a867ec5764952
neivyx <janeivyhilario@gmail.com>
2020-07-11 04:26:10 -0400

    initialize repo

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in puerto_rico_facts.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
---
+++ lib/puerto_rico_facts.rb
@@ -0,0 +1,6 @@
+require "puerto_rico_facts/version"
+
+module PuertoRicoFacts
+  class Error < StandardError; end
+  # Your code goes here...
+end
