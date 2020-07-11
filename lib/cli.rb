caba2a802611ca0a18871af282e3133e36eb9145
neivyx <janeivyhilario@gmail.com>
2020-07-11 05:23:48 -0400

    added methods scope notes

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,16 @@
 class CLI
     def start
-        puts "hello world!"
+        puts "Welcome "
+
+        #welcome users
+        #ask question
+        #provide input for users
+        #scrape facts call on scraper class
+        #print a random fact from scrape class
+        #more than one line to do a action, reduce to a method
+    end
+
+    def print_fact
+
     end
 end
