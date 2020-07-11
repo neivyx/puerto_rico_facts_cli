041f2daf88c1f9a6fdc69af251bd279e7b6e1dda
neivyx <janeivyhilario@gmail.com>
2020-07-11 19:57:20 -0400

    wrote out interface structure

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,16 +1,34 @@
 class CLI
-    def start
-        puts "Welcome "
 
-        #welcome users
-        #ask question
-        #provide input for users
-        #scrape facts call on scraper class
-        #print a random fact from scrape class
-        #more than one line to do a action, reduce to a method
+    def initialize
+        input = ""
+    end
+
+    def call
+        #start
+        #print_fact
+        #goodbye
+        Scaper.make_fact
+    end
+
+    def start
+        puts "Welcome"
+        #puts "small bio about puerto rico"
+        #puts "Would you like to know"
+        #ask question to print fact
     end
 
     def print_fact
+        #pull scrape / print new fact
+
+
+    end
+
+
+    def goodbye
+        #Ask if they would like another fact
+        #loop back to
+        #print_fact
 
     end
 end
