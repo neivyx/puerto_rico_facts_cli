2db5f70a83b937db57544347e2b6fecb857bad52
neivyx <janeivyhilario@gmail.com>
2020-07-11 06:38:34 -0400

    accessed scrapped data

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,26 @@
-require 'pry'
+require "nokogiri"
+require "pry"
+require "open-uri"
+
+puts "hello"
 
 class Scraper
+    puts "hello"
 
+    puts "is this working"
     def self.scrape_facts
         facts_page = Nokogiri::HTML(open("https://nationfacts.net/puerto-rico-facts/"))
-        binding.pry
-    end
+        # tableData = facts_page.css('table')
+        tableData = facts_page.css('table').text
+        tableData.each do |facts|
+            puts facts.text.strip
+
+            binding.pry
+        end
+       
+    
+    end 
 
 end 
+
+
